716f4332410239371490824eaa9d3bcabad33181
Matheus de Sousa <mhenrique.sousa43@gmail.com>
2023-07-04 06:11:30 -0300

    arrumando bugs

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,121 +1,190 @@
+import { useState, useEffect } from "react";
 import Logo from "../../assets/logo.svg";
-import Button from "../Button/Button";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
 import { NavLink } from "react-router-dom";
-import Offcanvas from "react-bootstrap/Offcanvas";
-import PopoverComponent from "../Popover/Popover";
+import { faUser, faTrashCan } from "@fortawesome/free-regular-svg-icons";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
-import { faUser } from "@fortawesome/free-regular-svg-icons";
-import Card from "../Card/Card";
-import BMT from "../../assets/Itens/Subs/15/frangoSuperBacon.png";
+import { Card, Container, Nav, Navbar, Offcanvas, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import "./HeaderStyles.css";
+import Icon from "../Icon/Icon";
+import ButtonSubway from "../Button/Button";
+import Popover from "../Popover/Popover"
+
+
 
 function Header() {
+  const [showCartOffCanvas, setShowCartOffCanvas] = useState(false);
+  const [ShoppingCart, setShoppingCart] = useState([]);
+
+  useEffect(() => {
+    const data = sessionStorage.getItem("ShoppingCart");
+    if (data) {
+      setShoppingCart(JSON.parse(data));
+    } else {
+      setShoppingCart([]);
+    }
+  }, [showCartOffCanvas]);
+
+  const handleCloseCartOffCanvas = () => setShowCartOffCanvas(false);
+  const handleShowCartOffCanvas = () => setShowCartOffCanvas(true);
+
+  // Função para deletar item do carrinho
+  const deleteItem = (name) => {
+    const newShoppingCart = ShoppingCart.filter((item) => item.itemName !== name);
+    setShoppingCart(newShoppingCart);
+    sessionStorage.setItem("ShoppingCart", JSON.stringify(newShoppingCart));
+  };
+
+
   return (
-    <Navbar bg="white" expand="lg" sticky="top">
-      <Container>
-        <Navbar.Brand href="/">
-          <img alt="Logo Subway" src={Logo} height="24" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-lg`} />
-        <Navbar.Offcanvas
-          id={`offcanvasNavbar-expand-lg`}
-          aria-labelledby={`offcanvasNavbarLabel-expand-lg}`}
-          placement="end"
-        >
-          <Offcanvas.Header closeButton className="borderBottom">
-            <a href="/">
-              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>
-                Subway
-              </Offcanvas.Title>
-            </a>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-            <Nav className="me-auto ms-auto borderBottom mb-xl-0 mb-lg-0 mb-md-4 mb-4">
-              <NavLink to="/menu" className="nav-link" activeclassname="active">
-                Cardápio
-              </NavLink>
-              <NavLink
-                to="/aboutUs"
-                className="nav-link"
-                activeclassname="active"
+    <>
+      <Navbar bg="white" expand="lg" sticky="top">
+        <Container>
+          <Link to="/">
+            <Navbar.Brand>
+              <img alt="Logo Subway" src={Logo} height="24" />
+            </Navbar.Brand>
+          </Link>
+          <div className="navBarIcons">
+            <div className="navNavToggle">
+              <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-lg`} />
+              <Navbar.Offcanvas
+                id={`offcanvasNavbar-expand-lg`}
+                aria-labelledby={`offcanvasNavbarLabel-expand-lg}`}
+                placement="start"
               >
-                Sobre nós
-              </NavLink>
-              <NavLink
-                to="/franchise"
-                className="nav-link"
-                activeclassname="active"
-              >
-                Franquias
-              </NavLink>
-            </Nav>
-            <PopoverComponent
-              icon={faShoppingCart}
-              content={
-                <Card
-                  cardWidth="10rem"
-                  title="B.M.T Italiano"
-                  scr={BMT}
-                  alt="B.M.T Italiano"
-                  cardClass="text-center"
-                  bodyClass="p-0"
-                  bdRadius="16px"
-                />
-              }
-            />
-            
-            <PopoverComponent
-              icon={faUser}
-              content={
-                <>
-                  <Link
-                    to="/login"
-                    className="nav-link"
-                    activeClassName="active"
-                  >
-                    Entrar
+                <Offcanvas.Header closeButton className="borderBottom">
+                  <a href="/">
+                    <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>
+                      Subway
+                    </Offcanvas.Title>
+                  </a>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                  <Nav className="me-auto ms-auto borderBottom mb-xl-0 mb-lg-0 mb-md-4 mb-4 me-5">
+                    <NavLink
+                      to="/menu"
+                      className={(navData) => (navData.isActive ? "nav-link active-style" : 'nav-link')}
+                    >
+                      Cardápio
+                    </NavLink>
+                    <NavLink
+                      to="/aboutUs"
+                      className={(navData) => (navData.isActive ? "nav-link active-style" : 'nav-link')}
+                    >
+                      Sobre nós
+                    </NavLink>
+                    <NavLink
+                      to="/franchise"
+                      className={(navData) => (navData.isActive ? "nav-link active-style" : 'nav-link')}
+                    >
+                      Franquias
+                    </NavLink>
+                  </Nav>
+                  <Link to="/menu" className="link">
+                    <ButtonSubway
+                      btnType="button"
+                      content="Fazer pedido"
+                      bgColor="var(--yellow-800)"
+                      border="1px solid var(--yellow-800)"
+                      color="var(--light)"
+                      padding=".5rem 2rem"
+                      bdRadius="200px"
+                      fontWeight="700"
+                      bgColorHover="var(--yellow-900)"
+                      borderHover="1px solid var(--yellow-900)"
+                    />
                   </Link>
-                  <hr />
-                  <Link
-                    to="/register"
-                    className="nav-link"
-                    activeClassName="active"
-                  >
-                    Cadastrar
-                  </Link>
-                  <hr />
-                  <Link
-                    to="/profile"
-                    className="nav-link"
-                    activeClassName="active"
-                  >
-                    Perfil
-                  </Link>
-                  <hr />
-                </>
-              }
-            />
-            <a href="/menu">
-              <Button
-                btnType="button"
-                content="Fazer pedido"
-                bgColor="var(--yellow-800)"
-                border="1px solid var(--yellow-800)"
-                color="var(--light)"
-                padding=".5rem 2rem"
-                bdRadius="200px"
-                fontWeight="700"
-                bgColorHover="var(--yellow-900)"
-                borderHover="1px solid var(--yellow-900)"
+                </Offcanvas.Body>
+              </Navbar.Offcanvas>
+            </div>
+            <div className="headerIcons">
+              <Popover
+                icon={faUser}
+                content={
+                  <>
+                    <Link to="/login" className="link">
+                      Entrar
+                    </Link>
+                    <hr />
+                    <Link
+                      to="/register"
+                      className="link"
+                    >
+                      Cadastrar
+                    </Link>
+                    <hr />
+                    <Link
+                      to="/profile"
+                      className="link"
+                    >
+                      Perfil
+                    </Link>
+                    <hr />
+                  </>
+                }
               />
-            </a>
-          </Offcanvas.Body>
-        </Navbar.Offcanvas>
-      </Container>
-    </Navbar>
+              <Button
+                variant="outline-secondary"
+                onClick={handleShowCartOffCanvas}
+                className="btn"
+                aria-controls={`offcanvasCart-expand-lg`}
+              >
+                <Icon icon={faShoppingCart} iconSize="lg" />
+              </Button>
+            </div>
+          </div>
+        </Container>
+      </Navbar>
+
+      <Offcanvas
+        show={showCartOffCanvas}
+        onHide={handleCloseCartOffCanvas}
+        placement="end"
+      >
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Carrinho de compras</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          {ShoppingCart.length === 0 ? (
+              <span>O carrinho está vazio.</span>
+          ) : (
+            ShoppingCart.map((item) => (
+              <Card className="mb-3" key={item.id}>
+                <Card.Body>
+                  <Card.Title>{item.itemName}</Card.Title>
+                  {item.size ? (
+                    <div className="itemResume d-flex flex-column">
+                      <small className="mb-1 text-muted">
+                        Tamanho: {item.size}
+                      </small>
+                      <small className="mb-1 text-muted">
+                        Pão: {item.bread}
+                      </small>
+                      <small className="mb-1 text-muted">
+                        Tamanho: {item.cheese}
+                      </small>
+                    </div>
+                  ) : (
+                    " "
+                  )}
+                  <small></small>
+                  <small>R${" "}{item.itemPrice.toLocaleString("pt-BR", {
+                    minimumFractionDigits: 2,
+                  })}</small>
+                  <Button
+                    className="deleteIcon"
+                    onClick={() => deleteItem(item.itemName)}
+                  >
+                    <Icon icon={faTrashCan} iconSize="lg" />
+                  </Button>
+                </Card.Body>
+              </Card>
+            ))
+          )}
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
   );
 }
 
